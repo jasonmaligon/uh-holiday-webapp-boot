044389e66e90daae0b0de103f2fc5a57f8a4dcdf
jmaligon <jason.maligon@gmail.com>
2020-01-03 09:44:06 -1000

    Fix merge conflicts.

--- src/main/java/edu/hawaii/its/holiday/controller/HolidayRestController.java
+++ src/main/java/edu/hawaii/its/holiday/controller/HolidayRestController.java
@@ -7,16 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> upstream/master
 
 import edu.hawaii.its.holiday.service.HolidayService;
 import edu.hawaii.its.holiday.type.Holiday;
@@ -146,11 +142,7 @@ public class HolidayRestController {
     public Page<Holiday> findPaginated(
             @RequestParam(value = "page") int page,
             @RequestParam(value = "size") int size) {
-<<<<<<< HEAD
-        logger.info("Entered REST holidays grid...");
-=======
         logger.debug("Entered REST holidays grid...");
->>>>>>> upstream/master
         return holidayService.findPaginatedHdays(page, size);
     }
 }
